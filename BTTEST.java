c1b28c3f452012cadacba36ba71e146d765dad8c
adv3k <78943781+adv3k@users.noreply.github.com>
2023-04-10 20:33:09 -0400

    Update and rename BSTTEST.java to BTTEST.java

--- BTTEST.java
+++ BTTEST.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.*;
 
-public class BSTTEST {
+public class BTTEST {
     public static void main(String[] args)  {
         //get list of numbers from file
         try {
-            File file = new File("bsttest.txt");
+            File file = new File("treetest.txt");
             Scanner scan = new Scanner(file);
             ArrayList<Integer> arr = new ArrayList<Integer>();
 
             for(String item: scan.nextLine().split(",")) {
                 arr.add(Integer.parseInt(item));
             }
-            //System.out.println(arr);
+            System.out.println(arr);
 
             BT<Integer> treetest = new BT<>();
             for (Integer item: arr) {
                 treetest.add(item);
             }
-            System.out.println(treetest.toString());
             
+            System.out.println(treetest.toString());
+            System.out.println(treetest.inOrder());
+            System.out.println(treetest.preOrder());
+            System.out.println(treetest.balancedTree(arr, 0, arr.size()-1));
+
             scan.close();
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
-        
-
-        //do inorder traversal and put into array (then print)
-        //read arr to create a balanced BST
-        //perform a preorder traversal on the balanced BST and then print
     }
+
 }
